fa8a4aa6ec4f27dc448b068e8ea8b68d9ebe70b1
vtate-812 <vaishnaviptate2003@gmail.com>
2026-04-22 10:12:57 +0530

    edit

--- jenkin-prac/Backend/app.py
+++ jenkin-prac/Backend/app.py
@@ -6,21 +6,27 @@ CORS(app)
 
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    data = request.json
-    a = float(data["num1"])
-    b = float(data["num2"])
-    op = data["operation"]
+    try:
+        data = request.json
+        a = float(data["num1"])
+        b = float(data["num2"])
+        op = data["operation"]
 
-    if op == "add":
-        res = a + b
-    elif op == "sub":
-        res = a - b
-    elif op == "mul":
-        res = a * b
-    
-    else:
-        return jsonify({"error": "Invalid"})
+        if op == "add":
+            res = a + b
+        elif op == "sub":
+            res = a - b
+        elif op == "mul":
+            res = a * b
+        elif op == "div":
+            res = a / b
+        else:
+            return jsonify({"error": "Invalid operation"})
 
-    return jsonify({"result": res})
+        return jsonify({"result": res})
 
-app.run(host="0.0.0.0", port=5000)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
